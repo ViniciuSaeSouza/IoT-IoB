c535b41a16d51190450bf19ba72b1fd1ea12c278
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-19 11:56:05 -0300

    aula 02

---
+++ aula00/aula_01/sketch.ino
@@ -0,0 +1,33 @@
+/*
+Arduino UNO
+
+Linguagem: Wiring (C++ adaptado para microcontroladores)
+
+Portas digitais (pinos de 0 a 13):
+Pode ser 
+Input (entrada) ---> botão, sensor digital
+Output (saída) ---> LED, display, relé, buzzer
+0volts ----> 0 ----> Low ----> false
+ou
+5volts ----> 1 ----> High ----> true
+
+Exemplo:
+O LED ligado ao pino 5 deverá piscar em intervalos de meio segundo.
+
+*/ 
+
+#define LED 5
+// Opções:
+// const int LED = 5;
+// int LED = 5
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
